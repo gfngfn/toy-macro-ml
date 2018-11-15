0d14e796163eccd584d4f7efe1bc0ebc22f06897
Takashi Suwa <bd.gfngfn@gmail.com>
2018-11-15 16:35:53 +0900

    omit levels

--- src/typechecker.ml
+++ src/typechecker.ml
@@ -27,7 +27,7 @@ let unify tyact tyexp =
   if res then () else raise (ContradictionError(tyact, tyexp))
 
 
-let rec aux lev tyenv (rng, utastmain) =
+let rec aux tyenv (rng, utastmain) =
   match utastmain with
   | Int(_) -> (rng, BaseType(IntType))
   | Bool(_) -> (rng, BaseType(BoolType))
@@ -40,12 +40,12 @@ let rec aux lev tyenv (rng, utastmain) =
       end
 
   | Lambda(((rngv, x), tydom), utast0) ->
-      let tycod = aux lev (tyenv |> Typeenv.add x tydom) utast0 in
+      let tycod = aux (tyenv |> Typeenv.add x tydom) utast0 in
       (rng, FuncType(tydom, tycod))
 
   | Apply(utast1, utast2) ->
-      let ty1 = aux lev tyenv utast1 in
-      let ty2 = aux lev tyenv utast2 in
+      let ty1 = aux tyenv utast1 in
+      let ty2 = aux tyenv utast2 in
       begin
         match ty2 with
         | (_, FuncType(tydom, tycod)) ->
@@ -58,25 +58,25 @@ let rec aux lev tyenv (rng, utastmain) =
       end
 
   | If(utast0, utast1, utast2) ->
-      let ty0 = aux lev tyenv utast0 in
+      let ty0 = aux tyenv utast0 in
       unify ty0 (Range.dummy "If", BaseType(BoolType));
-      let ty1 = aux lev tyenv utast1 in
-      let ty2 = aux lev tyenv utast2 in
+      let ty1 = aux tyenv utast1 in
+      let ty2 = aux tyenv utast2 in
       unify ty1 ty2;
       ty1
 
   | LetIn(((_, x), ty1), utast1, utast2) ->
-      let ty2 = aux lev (tyenv |> Typeenv.add x ty1) utast2 in
+      let ty2 = aux (tyenv |> Typeenv.add x ty1) utast2 in
       ty2
 
   | LetRecIn(((rngv, x), tyf), utast1, utast2) ->
       let tyenv = tyenv |> Typeenv.add x tyf in
-      let ty1 = aux (lev + 1) tyenv utast1 in
+      let ty1 = aux tyenv utast1 in
       unify ty1 tyf;
-      let ty2 = aux lev tyenv utast2 in
+      let ty2 = aux tyenv utast2 in
       ty2
 
 
 let main utast =
   let tyenv = Primitives.initial_type_environment in
-  aux 0 tyenv utast
+  aux tyenv utast
